6a6c8431b5d9048bd1dc9fae9ddb4a76482e083a
DrFreddy <fre_rz@hotmail.com>
2015-09-10 15:31:51 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,39 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructura de datos
+ * Laboratorio #7
+ * @author Freddy José Ruíz 14592
+ *
+ */
+public class main{
+
+	public static void main(String[] args){
+		Lectura traductor = new Lectura();
+		System.out.println ("BIENVENIDO AL DICCIONARIO INGLÉS-ESPAÑOL");
+		System.out.println("A continuación traduciremos el texto ingresado...");
+		System.out.println("Este es el texto en inglés:");
+		
+		//Lectura del diccionario
+		try {
+			traductor.lecturaAsociacion("src\\diccionario.txt");
+			}
+			catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+		
+		//Lectura del texto
+		try {
+			traductor.lecturaTraduccion("src\\texto.txt");
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Muestra de los datos del diccionario inOrder
+		traductor.inOrder();
+	
+		
+	}
+
+}
